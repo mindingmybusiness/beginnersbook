025cda704964fe6d9831c384f5384247b5bef979
Cubimal Chik <shubhachakrala@gmail.com>
2023-05-07 12:59:35 +0530

    two dimensional arrays

---
+++ src/arrays/TwoDimensionalArrays.java
@@ -0,0 +1,17 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class TwoDimensionalArrays {
+
+	public static void main(String[] args) {
+		int[][] ourArray = {
+				{1,2,3,4},
+				{2,3,4,5},
+				{3,4,5,6}
+		};
+		
+		System.out.println(Arrays.deepToString(ourArray));
+		System.out.println();
+	}
+}
